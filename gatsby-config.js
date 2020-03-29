7fda3a0a7fbfaadc8b33547b1cba84c618f1b0eb
Brenden Walters <brendenwalters42@gmail.com>
2020-03-29 17:08:35 -0400

    Added nav links with Anchor Link plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,14 @@ module.exports = {
       resolve: `gatsby-source-instagram`,
       options: {
         username: `brenden_walters`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-anchor-links`,
+      options: {
+        offset: -100
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,10 @@
 import { Link } from "gatsby"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header >
+  <header>
     <div
       style={{
         margin: `0 auto`,
@@ -11,17 +12,51 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+      <div className="navigation-container">
+        <AnchorLink
+          to="/#about-me"
+          title="About"
+          className="nav-link"
+          stripHash
+        />
+        <AnchorLink
+          to="/#portfolio"
+          title="Portfolio"
+          classname="nav-link"
+          stripHash
+        />
+        <AnchorLink
+          to="/#experience"
+          title="Experience"
+          className="nav-link"
+          stripHash
+        />
+        <AnchorLink 
+          to="/#photography"
+          title="Photography"
+          className="nav-link"
+          stripHash
+        />
+        <AnchorLink 
+          to="/#contact"
+          title="Contact"
+          className="nav-link"
+          stripHash
+        />
+      </div>
     </div>
   </header>
 )
