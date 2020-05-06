8bcc101542a67c5702f445bb62a39e176a291274
Brenden Walters <brendenwalters42@gmail.com>
2020-05-05 22:07:07 -0400

    Start on About card

--- src/components/aboutModule/About.js
+++ src/components/aboutModule/About.js
@@ -1,10 +1,36 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Styles from "./About.module.scss"
+
 
 const About = () => {
+
+  const Data = useStaticQuery(graphql`
+    query aboutPortraitQuery {
+      file(relativePath: {eq: "portrait-ph.jpg"}) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      
+    }
+  `)
+
   return (
-    <div className="about-section" name="about-me" id="about-me">
-      <h2>About</h2>
-      This is where I will write about what I do with my life.  I can include details about photography and making stuff for the internet.  For now, I am just leaving this placeholder content 
+    <div className={Styles.aboutSection} name="about-me" id="about-me">
+      <div className={Styles.aboutCard}>
+        <div className={Styles.aboutPortrait}>
+          <Img fluid={Data.file.childImageSharp.fluid} />
+        </div>
+        <div>
+          <h2>About</h2>
+          <p>This is where I will write about what I do with my life.  I can include details about photography and making stuff for the internet.  For now, I am just leaving this placeholder content </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/heroModule/Hero.js
+++ src/components/heroModule/Hero.js
@@ -3,7 +3,7 @@ import Styles from "./Hero.module.scss"
 
 const Hero = (props) => {
   return (
-    <div className={`${Styles.hero} hero is-fullheight`}>
+    <div className={`${Styles.hero} hero `}>
       <div className="hero-body">
         <div className="container has-text-centered">
           <h1 className="title">{props.title}</h1>
