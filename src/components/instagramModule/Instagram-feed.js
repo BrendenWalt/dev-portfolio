0886e0a139246a4e40607b41915e497ccf924821
Brenden Walters <brendenwalters42@gmail.com>
2020-05-12 22:06:09 -0400

    Instagram overlay and grid structure

--- src/components/instagramModule/Instagram-feed.js
+++ src/components/instagramModule/Instagram-feed.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { FaInstagram } from "react-icons/fa"
 
 import Styles from "./Instagram.module.scss"
 
@@ -22,13 +23,21 @@ const Instagram = () => {
   const feedSort = feed.allInstaNode.edges.sort((a,b) => b.node.timestamp - a.node.timestamp).slice(0,6);
   return (
     <div className={Styles.instagramFeed}>
-      <div className={Styles.instagramGrid}>
+      <div className={Styles.instagramGrid} data-igoverlay="">
         {feedSort.map(({ node }) => (
           <div key={node.id} className={Styles.instagramItem}>
             <img src={node.preview} alt="" />
           </div>
         ))}
+        <div className={Styles.instagramOverlay}>
+          <h3>
+            <a href="">
+              <FaInstagram /> | Follow me on instagram
+            </a>
+          </h3>
+        </div>
       </div>
+      
     </div>
   )
 }
