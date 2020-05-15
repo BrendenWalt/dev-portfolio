3c0685f78690430ada4ea7641ef74081a5e1ea17
Brenden Walters <brendenwalters42@gmail.com>
2020-05-15 16:29:26 -0400

    Minor style changes to header and contact module

--- src/components/contactModule/Contact.js
+++ src/components/contactModule/Contact.js
@@ -1,30 +1,35 @@
-import React from "react"
+import React from "react";
+
+import Styles from "./Contact.module.scss";
 
 const Contact = () => {
   return (
-    <div className="contact-form" name="contact" id="contact">
-      <form action="https://formspree.io/xgevdnrk" method="post">
-        <label htmlFor="name">
-          Name
+    <div className={Styles.contactModule}>
+      <div className={Styles.contactForm} name="contact" id="contact">
+        <form action="https://formspree.io/xgevdnrk" method="post">
+          <label htmlFor="name">
+            Name
           <input type="text" id="name" name="name" />
-        </label>
-        <label>
-          Email
+          </label>
+          <label>
+            Email
           <input type="email" name="_replyto" id="email" />
-        </label>
-        <label>
-          Subject
+          </label>
+          <label>
+            Subject
           <input type="text" name="subject" id="subject" />
-        </label>
-        <label>
-          Message
+          </label>
+          <label>
+            Message
           <textarea name="message" id="message" rows="5" />
-        </label>
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-        <input type="text" name="_gotcha" style={{ display:`none` }} />
-      </form>
+          </label>
+          <button type="submit">Send</button>
+          <input type="reset" value="Clear" />
+          <input type="text" name="_gotcha" style={{ display: `none` }} />
+        </form>
+      </div>
     </div>
+    
   )
 }
 
