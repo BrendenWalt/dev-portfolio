08de73e4cd6424969eec630b18d45856e5198740
Brenden Walters <brendenwalters42@gmail.com>
2020-05-20 18:19:05 -0400

    Adding company logos to resumeitems.json

--- src/components/experienceModule/Experience.js
+++ src/components/experienceModule/Experience.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Styles from "./Experience.module.scss"
 
 const Experience = () => {
   const data = useStaticQuery(
@@ -13,6 +16,13 @@ const Experience = () => {
             description
             id
             job_title
+            company_logo {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
@@ -21,13 +31,14 @@ const Experience = () => {
   )
   console.log(data);
   return (
-    <div className="experience-section content-module" name="experience" id="experience">
+    <div className={`${Styles.experienceModule} content-module`} name="experience" id="experience">
       <h2>Experience</h2>
-      <div className="experience-list">
+      <div className={Styles.experienceList}>
         {data.allResumeItemsJson.edges.map(({node}) => (
-          <div className="experience-item" key={node.id}>
+          <div className={Styles.experienceItem} key={node.id}>
             <div className="experience-title">{node.job_title}</div>
             <div className="experience-company">{node.company}</div>
+            <Img fluid={node.company_logo.childImageSharp.fluid} />
             <div className="experience-range">{node.date_range}</div>
             <div className="experience-description">{node.description}</div>
           </div>
