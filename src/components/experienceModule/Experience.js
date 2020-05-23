33698fc2b03b8f9951e2e5479bbb2d7971a0225d
Brenden Walters <brendenwalters42@gmail.com>
2020-05-23 09:51:32 -0400

    Experience Cards styles

--- src/components/experienceModule/Experience.js
+++ src/components/experienceModule/Experience.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
 
 import Styles from "./Experience.module.scss"
 
@@ -18,7 +19,7 @@ const Experience = () => {
             job_title
             company_logo {
               childImageSharp {
-                fluid {
+                fluid(quality: 80, maxWidth: 200) {
                   ...GatsbyImageSharpFluid
                 }
               }
@@ -34,15 +35,18 @@ const Experience = () => {
     <div className={`${Styles.experienceModule} content-module`} name="experience" id="experience">
       <h2>Experience</h2>
       <div className={Styles.experienceList}>
-        {data.allResumeItemsJson.edges.map(({node}) => (
-          <div className={Styles.experienceItem} key={node.id}>
-            <div className="experience-title">{node.job_title}</div>
-            <div className="experience-company">{node.company}</div>
-            <Img fluid={node.company_logo.childImageSharp.fluid} />
-            <div className="experience-range">{node.date_range}</div>
-            <div className="experience-description">{node.description}</div>
+        {data.allResumeItemsJson.edges.map(({node}) => {
+          const bg = { backgroundImage: `url(${node.company_logo.childImageSharp.fluid.src})`};
+          return (
+            <div className={Styles.experienceItem} key={node.id} style={bg}>
+            <h4 className={Styles.experienceTitle}>{node.job_title}</h4>
+            <p className={Styles.experienceCompany}>{node.company}</p>
+            <p className={Styles.experienceRange}>{node.date_range}</p>
+            <p className={Styles.experienceDescription}>{node.description}</p>
           </div>
-        ))}
+          )
+          
+        })}
       </div>
     </div>
   )
