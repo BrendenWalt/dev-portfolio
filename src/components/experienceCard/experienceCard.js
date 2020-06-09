9f8db5a56b24dbfeccb2e75e2ac4fd9b8f1c6da7
Brenden Walters <brendenwalters42@gmail.com>
2020-06-08 22:17:39 -0400

    Created separate module for experience Cards.

---
+++ src/components/experienceCard/experienceCard.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import { FaCaretRight } from 'react-icons/fa'
+import Styles from './experienceCard.module.scss'
+
+const ExperienceCard = (props) => {
+
+  const [active, setActive] = useState(false);
+
+  const toggleAccordion = (e) => {
+    e.preventDefault();
+    setActive(!active);
+    alert(active);
+  }
+
+  const info = props.info;
+
+  return (
+    <div className={Styles.experienceItem} key={info.id} style={props.bg}>
+      <h4 className={Styles.experienceTitle}>{info.job_title}</h4>
+      <p className={Styles.experienceCompany}>{info.company}</p>
+      <p className={Styles.experienceRange}>{info.date_range}</p>
+      <p className={Styles.experienceDescription}>{info.description}</p>
+      <div className={`${Styles.experienceAccordion}${active ? ' open' : ''}`} onClick={toggleAccordion} role="presentation">
+        <span className={Styles.accIcon}>
+          <FaCaretRight />
+        </span> More Details...
+              <div className={Styles.experienceAccordionContent}>
+          <ul className={Styles.experienceTraits}>
+            {info.list.map((item) => {
+              return (
+                <li>{item}</li>
+              )
+            })}
+          </ul>
+          <ul className={Styles.experienceSkills}>
+            {info.skills_list.map((skill) => {
+              return (
+                <li>{skill}</li>
+              )
+            })}
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default ExperienceCard
--- src/components/experienceModule/Experience.js
+++ src/components/experienceModule/Experience.js
@@ -1,12 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { FaCaretRight} from 'react-icons/fa'
+import ExperienceCard from '../experienceCard/experienceCard'
 import Img from "gatsby-image"
 import BackgroundImage from "gatsby-background-image"
 
 import Styles from "./Experience.module.scss"
 
 const Experience = () => {
+
+  
+
   const data = useStaticQuery(
     graphql`
     query {
@@ -33,6 +36,9 @@ const Experience = () => {
     }
     `
   )
+
+  
+
   console.log(data);
   return (
     <div className={`${Styles.experienceModule} content-module`} name="experience" id="experience">
@@ -41,39 +47,8 @@ const Experience = () => {
         {data.allResumeItemsJson.edges.map(({node}) => {
           const bg = { backgroundImage: `url(${node.company_logo.childImageSharp.fluid.src})`};
           return (
-            <div className={Styles.experienceItem} key={node.id} style={bg}>
-            <h4 className={Styles.experienceTitle}>{node.job_title}</h4>
-            <p className={Styles.experienceCompany}>{node.company}</p>
-            <p className={Styles.experienceRange}>{node.date_range}</p>
-            <p className={Styles.experienceDescription}>{node.description}</p>
-            <div className={Styles.experienceAccordion}>
-              <a href="#">
-                <span className={Styles.accIcon}>
-                  <FaCaretRight />
-                </span> More Details...
-              </a>
-              <div className={Styles.experienceAccordionContent}>
-                <ul className={Styles.experienceTraits}>
-                  {node.list.map((item) => {
-                    return (
-                      <li>{item}</li>
-                    )
-                  })}
-                </ul>
-                <ul className={Styles.experienceSkills}>
-                  {node.skills_list.map((skill) => {
-                    return (
-                      <li>{skill}</li>
-                    )
-                  })}
-                </ul>
-              </div>
-              
-            </div>
-           
-          </div>
+            <ExperienceCard info={node} bg={bg} />
           )
-          
         })}
       </div>
     </div>
