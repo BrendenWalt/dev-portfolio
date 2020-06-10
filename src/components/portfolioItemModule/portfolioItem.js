2deb021ec025949ad5366fcf0df4820e16d3c1ff
Brenden Walters <brendenwalters42@gmail.com>
2020-06-09 21:41:06 -0400

    Created portfolio item component

---
+++ src/components/portfolioItemModule/portfolioItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Styles from './portfolioItem.module.scss'
+
+const PortfolioItem = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PortfolioItem;
