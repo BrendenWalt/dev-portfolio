2117d48bce6802e9ef7fb3872a8d1f08320427eb
Brenden Walters <brendenwalters42@gmail.com>
2020-06-13 16:32:17 -0400

    Minor updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import Experience from "../components/experienceModule/Experience"
 import Photog from "../components/photogModule/Photog"
 import Contact from "../components/contactModule/Contact"
 import Instagram from "../components/instagramModule/Instagram-feed"
+import TechIcons from "../components/techIconsModule/TechIcons"
 import ToTop from "../components/totopModule/ToTop"
 
 const IndexPage = ({data}) => {
@@ -30,6 +31,7 @@ const IndexPage = ({data}) => {
         <PhotoDivider />
         <About />
         <Experience />
+        <TechIcons />
         <Photog />
         <Instagram />
         <Contact />
