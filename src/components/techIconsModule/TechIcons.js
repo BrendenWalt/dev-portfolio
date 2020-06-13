cc408e05bcb3891b8e00c2096c856c4a982989a9
Brenden Walters <brendenwalters42@gmail.com>
2020-06-13 16:31:46 -0400

    Added techIcons component

---
+++ src/components/techIconsModule/TechIcons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Styles from "./TechIcons.module.scss"
+
+const TechIcons = () => {
+  return (
+    <div className={Styles.techIconsModule} >
+    </div>
+  )
+}
+
+export default TechIcons;
