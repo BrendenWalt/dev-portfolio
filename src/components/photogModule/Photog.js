aebe95bb583ba8f5c1fcd4c9ae25bd4f7fa3695e
Brenden Walters <brendenwalters42@gmail.com>
2020-06-14 22:19:32 -0400

    photog module image import

--- src/components/photogModule/Photog.js
+++ src/components/photogModule/Photog.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Styles from "./Photog.module.scss"
 
 const Photog = () => {
+
+  const images = useStaticQuery(
+    graphql`
+      query {
+        allImageSharp(filter: {original: {src: {regex: "/photog-sample/"}}}) {
+          edges {
+            node {
+              fluid(quality: 80, maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  console.log(images.allImageSharp.edges);
+
   return (
-    <div className="photography-section content-module" name="photography" id="photography">
+    <div className={`${Styles.photogModule} content-module`} name="photography" id="photography">
       <h2>Photography</h2>
+      <div className={Styles.photogGrid}>
+        {images.allImageSharp.edges.map(({node}) => {
+          return (
+            <Img fluid={node.fluid} />
+          );
+        })}
+      </div>
     </div>
   )
 }
