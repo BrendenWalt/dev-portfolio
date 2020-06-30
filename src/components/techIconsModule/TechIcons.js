44583f3178b10d4d91415315574ca4b3a1170377
Brenden Walters <brendenwalters42@gmail.com>
2020-06-29 21:44:06 -0400

    Added content to techIcons component

--- src/components/techIconsModule/TechIcons.js
+++ src/components/techIconsModule/TechIcons.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaSass,
+  FaWordpress,
+  FaJsSquare,
+  FaReact,
+  FaAngular,
+  FaGithub,
+  FaPhp,
+  FaRebel
+} from "react-icons/fa"
+
 
 import Styles from "./TechIcons.module.scss"
 
 const TechIcons = () => {
   return (
-    <div className={Styles.techIconsModule} >
+    <div className={Styles.techIconsModule}>
+      <div className={Styles.techIcon}>
+        <FaHtml5 title="HTML5" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaJsSquare title="Javascript" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaReact title="ReactJS" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaAngular title="AngularJS" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaCss3Alt title="CSS" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaSass title="Sass" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaWordpress title="Wordpress" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaGithub title="Github" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaRebel title="Rebel Alliance" />
+      </div>
+      <div className={Styles.techIcon}>
+        <FaPhp title="PHP" />
+      </div>
     </div>
   )
 }
