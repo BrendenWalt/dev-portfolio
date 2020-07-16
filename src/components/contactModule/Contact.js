f3676f3050c306d03750b35a15b09ea52e760376
Brenden <brendenwalters42@gmail.com>
2020-07-15 22:14:12 -0400

    Updates to contact form

--- src/components/contactModule/Contact.js
+++ src/components/contactModule/Contact.js
@@ -11,53 +11,47 @@ const Contact = () => {
         <h2>Contact</h2>
       </div>
 
-      <div className={Styles.contactForm} name="contact" id="contact">
-        <form action="https://formspree.io/xgevdnrk" method="post" >
-          <div className={Styles.formGroup}>
+      <div className={Styles.formContainer}>
+        <div className={Styles.contactForm} name="contact" id="contact">
+          <form action="https://formspree.io/xgevdnrk" method="post" >
+            <div className={Styles.formGroup}>
 
-            <div className={Styles.formField}>
-              <label htmlFor="name">
-                Name*
-              </label>
-              <input type="text" id="name" name="name" />
-            </div>
+              <div className={Styles.formField}>
+                <label htmlFor="name">
+                  Name*
+                </label>
+                <input type="text" id="name" name="name" />
+              </div>
 
-            <div className={Styles.formField}>
-              <label htmlFor="email">
-                Email*
-              </label>
-              <input type="email" name="_replyto" id="email" />
-            </div>
+              <div className={Styles.formField}>
+                <label htmlFor="email">
+                  Email*
+                </label>
+                <input type="email" name="_replyto" id="email" />
+              </div>
 
-            <div className={Styles.formField}>
-              <label htmlFor="name">
-                Phone
-              </label>
-              <input type="tel" id="name" name="name" />
-            </div>
+              <div className={`${Styles.formField} ${Styles.formMessage}`}>
+                <label>
+                  Message*
+                <textarea name="message" id="message" rows="5" />
+                </label>
+              </div>
+              
+              <button type="submit" className="cta-secondary">Send Message</button>
 
-           
-          </div>
-         
-          
-          <div className={Styles.formGroup}>
-            <div className={Styles.formField}>
-              <label>
-                Message*
-              <textarea name="message" id="message" rows="5" />
-              </label>
+            
             </div>
             
-            <button type="submit" className="cta-primary">Send</button>
-          </div>
-          
-          <input type="text" name="_gotcha" style={{ display: `none` }} />
-        </form>
+            <input type="text" name="_gotcha" style={{ display: `none` }} />
+          </form>
+        </div>
+
+        <div className={Styles.contactSocial}>
+          <SocialMedia />
+        </div>
       </div>
 
-      <div className={Styles.contactSocial}>
-        <SocialMedia />
-      </div>
+      
     </div>
     
   )
